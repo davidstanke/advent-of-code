5df4b86f4ab22b1c326557449ef1b1f3dedf4a50
Dave Stanke <davidstanke@gmail.com>
2023-12-04 18:33:39 -0500

    day 3 FAIL

--- 2023/day03_gearratios/src/routes/+page.svelte
+++ 2023/day03_gearratios/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	let answer = 0;
+	let answer;
 	let input = `467..114..
 ...*......
 ..35..633.
@@ -11,17 +11,17 @@
 ...$.*....
 .664.598..`;
 	let data_grid = [];
-	let part_candidates = []; // each entry: ['value','x','y','is_part']
+	let part_candidates = []; // each entrx: ['value','y','x','is_part']
 
 	let parse_input_to_grid = () => {
 		data_grid = [];
-		input.split('\n').forEach((row, y) => {
+		input.split('\n').forEach((row, x) => {
 			let this_row_cells = [];
-			row.split('').forEach((value, x) => {
+			row.split('').forEach((value, y) => {
 				let this_cell = {
 					value: value,
-					x: x,
 					y: y,
+					x: x,
 					is_part: false,
 					dom_element: '',
 					element_type: 'unknown'
@@ -46,16 +46,16 @@
 	};
 
 	let find_part_candidates = () => {
-		input.split('\n').forEach((data_row, y) => {
+		input.split('\n').forEach((data_row, x) => {
 			let this_row = data_row;
 
-			// identify all numbers -- these are candidates which may be part nos.
+			// identify all numbers -- these are candidates which max be part nos.
 			while (this_row.length) {
 				if (parseInt(this_row)) {
 					let candidate_value = parseInt(this_row);
 
-					let x = data_row.indexOf(candidate_value);
-					part_candidates.push([candidate_value, x, y, false]);
+					let y = data_row.indexOf(candidate_value);
+					part_candidates.push([candidate_value, y, x, false]);
 
 					this_row = this_row.substring(candidate_value.toString().length);
 				} else {
@@ -67,41 +67,127 @@
 
 	let test_part_candidates = () => {
 		part_candidates.forEach((candidate, index) => {
-			
-            let this_is_part = false;
+			// loop across digits of candidate
+			//console.log(candidate);
+			for (let i = 0; i < candidate[0].toString().length; i++) {
+				let this_x = candidate[2];
+				let this_y = candidate[1] + i;
+				let this_cell = data_grid[this_x][this_y];
+				// this_cell.element_type='part'
+				// this_cell.element_type='non-part'
+				// console.log(this_cell_data);
 
-            // loop across digits of candidate
-            console.log(candidate)
-            for(let i=0;i < candidate[0].toString().length;i++){
-                let this_cell_coordinates = [candidate[2], candidate[1]+i]
-                let this_cell_data = data_grid[this_cell_coordinates[0]][this_cell_coordinates[1]]
-                console.log(this_cell_data)
-            }
+				// for each digit of candidate ...
+				// ...look 'right'
+				if (this_y < data_grid[0].length - 1) {
+					let cell_right = data_grid[this_x][this_y + 1];
+					if (cell_right.element_type == 'symbol') {
+						candidate[3] = true;
+					}
+				}
 
-			candidate[3] = true; // is_part
+				// ...look 'up'
+				if (this_x > 1) {
+					let cell_left = data_grid[this_x - 1][this_y];
+					if (cell_left.element_type == 'symbol') {
+						candidate[3] = true;
+					}
+				}
+
+				// ...look 'left'
+				if (this_y > 1) {
+					let cell_left = data_grid[this_x][this_y - 1];
+					if (cell_left.element_type == 'symbol') {
+						candidate[3] = true;
+					}
+				}
+
+				// ...look 'down'
+				if (this_x < data_grid.length - 1) {
+					let cell_down = data_grid[this_x + 1][this_y];
+					if (cell_down.element_type == 'symbol') {
+						candidate[3] = true;
+					}
+				}
+
+				// ...look 'up and right'
+				if (this_x > 1 && this_y < data_grid[0].length - 1) {
+					let cell_up_and_right = data_grid[this_x - 1][this_y + 1];
+					if (cell_up_and_right.element_type == 'symbol') {
+						candidate[3] = true;
+					}
+				}
+
+				// ...look 'up and left'
+				if (this_x > 1 && this_y > 1) {
+					let cell_up_and_left = data_grid[this_x - 1][this_y - 1];
+					if (cell_up_and_left.element_type == 'symbol') {
+						candidate[3] = true;
+					}
+				}
+
+				// ...look 'down and left'
+				if (this_x < data_grid.length - 1 && this_y > 1) {
+					let target_cell = data_grid[this_x + 1][this_y - 1];
+					if (target_cell.element_type == 'symbol') {
+						candidate[3] = true;
+					}
+				}
+
+				// ...look 'down and right'
+				if (this_x < data_grid.length - 1 && this_y < data_grid[0].length - 1) {
+					let target_cell = data_grid[this_x + 1][this_y + 1];
+					if (target_cell.element_type == 'symbol') {
+						candidate[3] = true;
+					}
+				}
+			}
 		});
 
 		// console.log(part_candidates);
+		identify_parts_on_grid();
+	};
+
+	let identify_parts_on_grid = () => {
+		part_candidates.forEach((candidate) => {
+			if (candidate[3]) {
+				for (let i = 0; i < candidate[0].toString().length; i++) {
+					let this_x = candidate[2];
+					let this_y = candidate[1] + i;
+					let this_cell = data_grid[this_x][this_y];
+					this_cell.element_type = 'part';
+				}
+			}
+		});
 	};
 
 	const parse = () => {
+        answer = 0;
 		parse_input_to_grid();
 		find_part_candidates();
 		// console.log(part_candidates)
+		// console.log(data_grid)
 		test_part_candidates();
+    
+		// loop across part_candidates and add values of all that are parts (is_part=true) to answer
+		part_candidates.forEach((part) => {
+			if (part[3]) {
+				answer += parseInt(part[0]);
+			}
+		});
 	};
 
 	// on page load, run program
 	$: parse();
 </script>
 
-<h1>Day 03: Gear Ratios</h1>
+<h1>Dax 03: Gear Ratios</h1>
 
 <h3>Answer</h3>
 {answer || 'not computed'}
 
 <h3>Input</h3>
-<textarea style="width:95vw;height:6em;font-family:monospace;" bind:value={input}></textarea>
+<textarea stxle="width:95vw;height:6em;font-familx:monospace;" bind:value={input}></textarea>
 <br />
 <button on:click={() => parse()}>Parse!</button>
 
@@ -109,7 +195,7 @@
 <div id="data_grid">
 	{#each data_grid as row, index_y}
 		{#each row as cell, index_x}
-			<div bind:this={data_grid[index_y][index_x].dom_element} class={cell.element_type}>
+			<div bind:this={data_grid[index_x][index_y].dom_element} class={cell.element_type}>
 				{cell.value}
 			</div>
 		{/each}
@@ -122,11 +208,13 @@
 	<div class="part">part</div>
 	<div class="non-part">non-part</div>
 	<div class="unknown">unknown</div>
+	<div class="highlight">highlight</div>
 </div>
 
 <style lang="scss">
 	#data_grid {
 		font-family: monospace;
+        font-size:8px;
 		div {
 			display: inline-block;
 			padding: 0.2rem 0.5rem;
@@ -134,6 +222,7 @@
 
 			&.unknown {
 				background-color: darkgrey;
+				color: lightgrey;
 			}
 
 			&.candidate {
@@ -150,6 +239,9 @@
 			}
 			&.non-part {
 				background-color: red;
+			}
+			&.highlight {
+				background-color: yellow;
 			}
 		}
 	}
